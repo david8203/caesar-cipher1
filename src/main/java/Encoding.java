ba173913463fce2745f5d0cf722cf3a33e30b262
David <kingdice@Atomic>
2022-04-23 12:56:33 +0300

    creates our constructor for the key and input text and set them private

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,2 +1,9 @@
 public class Encoding {
+    private int mKey;
+    private String mInputText;
+
+    public Encoding(String inputText,int key) {
+        mKey = key;
+        mInputText = inputText;
+    }
 }
