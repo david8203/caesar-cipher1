37e61a48a5328dec649af256299471ed32b761dd
David <kingdice@Atomic>
2022-04-23 13:02:59 +0300

    here  we write the logic to check whether the input is valid and also initialize our getters for the key and input text.

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -6,4 +6,22 @@ public class Encoding {
         mKey = key;
         mInputText = inputText;
     }
+    public String getInputText() {
+        return mInputText;
+    }
+    public int getKey()
+    {
+        return mKey;
+    }
+    public boolean isValidInputText()
+    {
+        char[] chars = mInputText.toCharArray();
+
+        for (char c : chars) {
+            if(!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
 }
