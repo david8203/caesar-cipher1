9a839e7675600b897d4d8f270cb5d411a4ecb30c
David <kingdice@Atomic>
2022-04-24 00:26:37 +0300

    here we are initializing our main class and creating a new instance of scanner to collect our user input data.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" What do you want to do here?\n 1. Encode \n 2. Decode  \n 3. Quit");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+    }
 }
