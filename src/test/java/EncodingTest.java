bdd60307d9d9538471266bf1a65d23062b4edae0
David <kingdice@Atomic>
2022-04-23 12:54:32 +0300

    we write our first encoding test to check whether it instantiates correctly and make sure the test fails

--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class EncodingTest {
+public class EncodingTest {
+    Encoding testEncoding = new Encoding("DAVID NDUNGU", 2);
+    @Test
+    public void checkIfItCreatesInstance()
+    {
+        assertEquals(true, testEncoding instanceof Encoding);
+    }
 
 }
