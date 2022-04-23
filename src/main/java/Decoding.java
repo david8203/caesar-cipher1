a891268a16dfb86ab9c273dd0bda579f8fc493b1
David <kingdice@Atomic>
2022-04-24 00:22:46 +0300

    here we are creating an if else statement to make sure our logic to decode is actually implemented in the implement stage

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -23,4 +23,40 @@ public class Decoding {
         }
         return true;
     }
+    public boolean isValidKey()
+    {
+        return mKey >1 && mKey < 26;
+    }
+
+    public String decode()
+    {
+        String decodedValue = "";
+
+        String encodedArray[] = mInputText.split("");
+
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for (int i = 0; i<encodedArray.length;i++)
+        {
+            if (mInputText.charAt(i) == ' ')
+            {
+                decodedValue += " ";
+            }
+            else{
+                int charPosition = alphabet.indexOf(mInputText.charAt(i));
+
+
+                int keyVal = (charPosition - mKey) % 26;
+                if (keyVal < 0)
+                {
+                    keyVal = alphabet.length() + keyVal;
+                }
+
+                char replaceValue = alphabet.charAt(keyVal);
+
+                decodedValue += replaceValue;
+            }
+        }
+        return decodedValue;
+    }
 }
