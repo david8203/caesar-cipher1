4de9d40e9ab7a84a1569d108b180eeac0d15bbe1
David <kingdice@Atomic>
2022-04-24 00:11:11 +0300

    here we create our constructor  and use our getters to fetch our key and input text

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,2 +1,15 @@
 public class Decoding {
+    private int mKey;
+    private String mInputText;
+
+    public Decoding(int key, String inputText) {
+        mKey = key;
+        mInputText = inputText;
+    }
+    public String getInputText() {
+        return mInputText;
+    }
+    public int getKey() {
+        return mKey;
+    }
 }
--- src/test/java/DecodingTest.java
+++ src/test/java/DecodingTest.java
@@ -1,5 +1,24 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecodingTest {
+    Decoding testDecoding = new Decoding( 2, "FCXKF PFWPIW");
+    @Test
+    public void checkIfSavesInputText()
+    {
+        assertEquals("FCXKF PFWPIW", testDecoding.getInputText() );
+    }
+    @Test
+    public void checkKeyIsSaved()
+    {
+        assertEquals(2,testDecoding.getKey());
+    }
+
+    @Test
+    public void isValidInputText()
+    {
+        assertEquals(true, testDecoding.isValidInputText());
+    }
 
 }
